c2432cf64acc838ea28d0ec48dda69ece553bcf6
FedericaC03 <fdrk03@hotmail.it>
2020-09-15 16:54:29 +0200

    fine esercizio

--- js/script.js
+++ js/script.js
@@ -1,16 +1,13 @@
-// Dare la possibilità all’utente attraverso 3 prompt di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
-
-
 //1 Creare un oggetto che descriva uno studente con le seguenti proprietà: nome, cognome e età. Stampare a schermo attraverso il for in tutte le proprietà.
-// var studente = {
-//   "nome" : 'Federica',
-//   "cognome": 'Cortese',
-//   "eta": '26',
-// };
-//
-// for (var key in studente){
-//   console.log(studente[key]);
-// }
+var studente = {
+  "nome" : 'Federica',
+  "cognome": 'Cortese',
+  "eta": '26',
+};
+
+for (var key in studente){
+  console.log(studente[key]);
+}
 
 
 //2 Creare un array di oggetti di studenti. Ciclare su tutti gli studenti e stampare per ognuno nome e cognome
@@ -41,3 +38,13 @@ for (var i = 0; i < studenti.length; i++) {
     console.log([key] + ": " + classe[key]);
   }
 }
+
+//3 Dare la possibilità all’utente attraverso 3 prompt di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
+var nuovoStudente = {};
+nuovoStudente.nome = prompt("Inserisci il tuo nome");
+nuovoStudente.cognome = prompt("Inserisci il tuo cognome");
+nuovoStudente.eta = prompt("Inserisci la tua età");
+// console.log(nuovoStudente);
+
+studenti.push(nuovoStudente);
+console.log(studenti);
