1443c43621decd9a4319ad2f876cbe1b2f61a86c
FedericaC03 <fdrk03@hotmail.it>
2020-09-15 16:28:11 +0200

    Secondo punto dell'esercizio completato

--- js/script.js
+++ js/script.js
@@ -2,15 +2,42 @@
 
 
 //1 Creare un oggetto che descriva uno studente con le seguenti proprietà: nome, cognome e età. Stampare a schermo attraverso il for in tutte le proprietà.
-var studente = {
-  "nome" : 'Federica',
-  "cognome": 'Cortese',
-  "eta": '26',
-};
-
-for (var key in studente){
-  console.log(studente[key]);
-}
+// var studente = {
+//   "nome" : 'Federica',
+//   "cognome": 'Cortese',
+//   "eta": '26',
+// };
+//
+// for (var key in studente){
+//   console.log(studente[key]);
+// }
 
 
 //2 Creare un array di oggetti di studenti. Ciclare su tutti gli studenti e stampare per ognuno nome e cognome
+var studenti =
+[
+ {
+   "nome" : 'Federica',
+   "cognome": 'Cortese',
+ },
+ {
+   "nome" : 'Avril',
+   "cognome": 'Lavigne',
+ },
+ {
+   "nome" : 'Miley',
+   "cognome": 'Cyrus',
+ },
+ {
+   "nome" : 'Lady',
+   "cognome": 'Gaga',
+ },
+];
+
+for (var i = 0; i < studenti.length; i++) {
+  // console.log(studenti[i]);
+  for (var key in studenti[i]) {
+    var classe = studenti[i]
+    console.log([key] + ": " + classe[key]);
+  }
+}
